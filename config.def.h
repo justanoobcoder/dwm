f426d1bd082ba39cb6a0ffa69ffbd6bce386c77e
justanoobcoder <syaorancode@gmail.com>
2021-08-09 15:20:18 +0700

    config.def.h: changed gaps to 10px

--- config.def.h
+++ config.def.h
@@ -5,10 +5,10 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 5;        /* horiz inner gap between windows */
-static const unsigned int gappiv    = 5;        /* vert inner gap between windows */
-static const unsigned int gappoh    = 5;        /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 5;        /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 10;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;        /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;        /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -149,7 +149,7 @@ static Key keys[] = {
     TAGKEYS(                        XK_7,                       6)
     TAGKEYS(                        XK_8,                       7)
     TAGKEYS(                        XK_9,                       8)
-    { MODKEY|ShiftMask,             XK_q,       quit,           SHCMD("dsysact") },
+    { MODKEY|ShiftMask,             XK_q,       spawn,          SHCMD("dsysact") },
     { MODKEY|ShiftMask,             XK_r,       quit,           {1} }, 
     { MODKEY,                       XK_Print,   spawn,          SHCMD("drecord") },
     { MODKEY,                       XK_Delete,  spawn,          SHCMD("drecord --kill") },
