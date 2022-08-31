9f351d89d25309ff2f2dffcc5f9b087c08f8def6
justanoobcoder <syaorancode@gmail.com>
2022-08-31 23:47:27 +0700

    config.def.h: used eww as statusbar, disabled smartgap

--- config.def.h
+++ config.def.h
@@ -10,11 +10,11 @@ static const unsigned int gappih    = 10;        /* horiz inner gap between wind
 static const unsigned int gappiv    = 10;        /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;        /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;        /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 1;        /* 1 means use non-dwm status bar */
-static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
+static const char *altbarclass      = "eww-bar"; /* Alternate bar class name */
 static const char *alttrayname      = "tray";    /* Polybar tray instance name */
 static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = { "monospace:size=10" };
