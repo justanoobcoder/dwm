a11e9b2009219a73f9729b0f03527bbda4016814
justanoobcoder <syaorancode@gmail.com>
2022-09-02 15:21:22 +0700

    patched dwm-vanitygaps

--- config.def.h
+++ config.def.h
@@ -5,6 +5,11 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -98,6 +103,22 @@ static const Key keys[] = {
 	{ 0,           MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ 0,           MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ 0,           MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ 0,            MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
+	{ 0,            MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
+	{ 0,            MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
+	{ 0,            MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
+	{ 0,            MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
+	{ 0,            MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
+	{ 0,            MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
+	{ 0,            MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ 0,            MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
+	{ 0,            MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
+	{ 0,            MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
+	{ 0,            MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
+	{ 0,            MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
+	{ 0,            MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
+	{ 0,            MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
+	{ 0,            MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 
 	{ 1,           MODKEY,                       XK_space,      setlayout,      {.v = &layouts[0]} },
 	{ 2,           MODKEY,                       XK_space,      setlayout,      {.v = &layouts[1]} },
